93e6d11d00807601b04c7b05048266d8acec7eb9
Owski <149725877+hyperficial@users.noreply.github.com>
2025-05-20 18:00:19 -0500

    Whatever message you want

---
+++ index.mjs
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
